613167174108772f4962df506c31af130788a4cb
akshayjadhav9423 <as.jadhav9423@gmail.com>
2026-01-07 20:24:29 +0530

    updated

---
+++ target/classes/static/README.md
@@ -0,0 +1,1 @@
+# satisfiestech.github.io
---
+++ target/classes/static/js/products.js
@@ -0,0 +1,13 @@
+//document.addEventListener("DOMContentLoaded", function () {
+//  fetch("/api/products")
+//    .then(response => response.json())
+//    .then(data => {
+//      const list = document.getElementById("product-list");
+//      data.forEach(product => {
+//        const item = document.createElement("li");
+//        item.innerHTML = `${product.name} - ₹${product.price}`;
+//        list.appendChild(item);
+//      });
+//    })
+//    .catch(error => console.error("Error fetching products:", error));
+//});
